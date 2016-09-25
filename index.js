07b25dbedc66427235f16209e73553f392f0248a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-09-25 10:01:03 +0200

    Create deviceState when new value arrive, exec and setup function now working

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 
 module.exports = function (sails) {
 
-    var init = require('./lib/zwave.init.js');
+    var connect = require('./lib/zwave.connect.js');
+    var disconnect = require('./lib/zwave.disconnect.js');
+    var exec = require('./lib/zwave.exec.js');
+    var setup = require('./lib/zwave.setup.js');
 
     gladys.on('ready', function(){
-        init();
+        connect();
     });
 
     return {
-
+        connect,
+        disconnect,
+        exec,
+        setup
     };
 };
